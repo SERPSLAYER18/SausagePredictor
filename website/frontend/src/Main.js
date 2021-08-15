a16edd7bf316afacb27c0ce24206781197dd451d
AlexZhamoydin <79421973+AlexZhamoydin@users.noreply.github.com>
2021-08-15 13:14:35 +0300

    cool frontend

--- website/frontend/src/Main.js
+++ website/frontend/src/Main.js
@@ -30,37 +30,48 @@ function Main() {
         <div className="App">
             <div className="container">
                 <div className="prikol">
-                    <h3>Predict "Vitalur" demand</h3>
+                    <h3 style={{textAlign: "center"}}>Predict "Vitalur" demand</h3>
 
+                    <label>Product id</label>
                     <fieldset>
-                        <input className="kekw" placeholder="Product name" type="text" tabIndex="1" name="product_"
+                        <input className="kekw" placeholder="Product id" type="text" tabIndex="1" name="product_"
                                required autoFocus
                                onChange={(e) => {
                                    setProduct(e.target.value)
                                }}/>
                     </fieldset>
+
+                    <label>Store id</label>
                     <fieldset>
-                        <input className="kekw" placeholder="Store address" type="text" tabIndex="2" name="address_"
+                        <input className="kekw" placeholder="Store id" type="text" tabIndex="2" name="address_"
                                required
                                onChange={(e) => {
                                    setAddress(e.target.value)
                                }}/>
                     </fieldset>
+
+                    <label>Comma separated values</label>
                     <fieldset>
                         <input className="kekw" placeholder="Values" type="text" tabIndex="3" name="values_" required
                                onChange={(e) => {
                                    setValues(e.target.value)
                                }}/>
                     </fieldset>
+
+                    <label>Date</label>
                     <fieldset>
                         <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
                     </fieldset>
+
+                    <label>Estimated consumption</label>
                     <fieldset>
                         <input className="kekw" placeholder="Prediction" type="text" tabIndex="3" name="values_"
                                disabled
                                value={result}/>
                     </fieldset>
-                    <button id="main_button" className="button-5" role="button" onClick={sendJSON}>Predict!</button>
+                    <div style={{textAlign: "center"}}>
+                        <button id="main_button" className="button-5" role="button" onClick={sendJSON}>Predict!</button>
+                    </div>
                     <p className="copyright">Designed by Team3</p>
 
                 </div>
